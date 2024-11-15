94926a35cb25b87d5e2d859405a36b040e3b81cf
Roshan Pandey <rpandox@gmail.com>
2024-11-15 14:36:17 +0545

    initialize tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',  // Vite's default entry point
+    './src/**/*.{js,jsx,ts,tsx}',  // Any JavaScript/TypeScript files in your src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
