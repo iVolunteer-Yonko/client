4cc665317b9065bb12ef514954fed9a5bd84e5e0
Roshan Pandey <rpandox@gmail.com>
2024-11-16 01:25:07 +0545

    mg conflict mero

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,7 +1,6 @@
 import {React, useContext} from 'react'
 import { LayoutContext } from './Layout'
 
-import React from 'react'
 import {HeroHome,AboutUs,AboutTheTeam,FAQ} from '../components/home'
 const Home = () => {
   return (
